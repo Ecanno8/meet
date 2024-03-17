ea0a7cf8b6807af132b4a6d60cee7ff330a58b28
Ecanno8 <ethancannon92@gmail.com>
2024-03-16 20:39:31 -0700

    wip

--- src/components/NumberOfEvents.js
+++ src/components/NumberOfEvents.js
@@ -5,6 +5,7 @@ const NumberOfEvents = ({ setCurrentNOE }) => {
     const handleInputChanged = (event) => {
         const value = event.target.value;
         setQuery(value);
+        setCurrentNOE(value);
     }
     return (
         <div id="number-of-events">
